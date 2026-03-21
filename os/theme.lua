a078a1df92af3ebaeba2c381662a9a9ec57f2778
Puntaro <punguitar1@gmail.com>
2026-03-21 15:21:18 +0200

    Add color palettes for various themes

--- os/theme.lua
+++ os/theme.lua
@@ -75,6 +75,14 @@ local themes = {
         btnClear   = colors.red,
         btnEqual   = colors.magenta,
         selected   = colors.magenta,
+        palette    = {
+            -- Dirty warm brown-green-yellow palette
+            [colors.magenta]   = 0xC4782A,  -- primary: warm coral-brown
+            [colors.pink]      = 0xD4A44C,  -- secondary/subtext: dirty yellow-gold
+            [colors.orange]    = 0x8B6914,  -- border: deep earthy brown
+            [colors.lightGray] = 0xB8A878,  -- text: warm off-white
+            [colors.lime]      = 0x7A9B4A,  -- success: muted olive green
+        },
     },
 
     -- ---- Copper -------------------------------------------------------------
@@ -98,6 +106,12 @@ local themes = {
         btnClear   = colors.red,
         btnEqual   = colors.lime,
         selected   = colors.yellow,
+        palette    = {
+            [colors.orange]    = 0xC87533,  -- primary: proper copper
+            [colors.yellow]    = 0xDAA520,  -- text/secondary: goldenrod
+            [colors.green]     = 0x4A7C59,  -- border: verdigris green
+            [colors.lime]      = 0x7CB98A,  -- subtext/success: light verdigris
+        },
     },
 
     -- ---- Toyota -------------------------------------------------------------
@@ -123,6 +137,13 @@ local themes = {
         btnClear   = colors.red,
         btnEqual   = colors.lightBlue,
         selected   = colors.lightBlue,
+        palette    = {
+            [colors.orange]    = 0xE8720C,  -- primary: clean automotive orange
+            [colors.gray]      = 0x5A6472,  -- border: cool steel gray
+            [colors.lightGray] = 0x9BA8B5,  -- text: lighter steel
+            [colors.lightBlue] = 0x4A90D9,  -- subtext/secondary: steel blue
+            [colors.cyan]      = 0x6BB8E0,  -- spec: lighter steel blue
+        },
     },
 
     -- ---- TARDIS -------------------------------------------------------------
@@ -148,6 +169,15 @@ local themes = {
         btnClear   = colors.red,
         btnEqual   = colors.cyan,
         selected   = colors.cyan,
+        palette    = {
+            [colors.black]     = 0x000814,  -- background: deep navy-black
+            [colors.gray]      = 0x1A3A5C,  -- border: dark blue-gray
+            [colors.blue]      = 0x1B4F8A,  -- subtext: deep TARDIS blue
+            [colors.lightBlue] = 0x3A8FCC,  -- secondary: mid cyan-blue
+            [colors.cyan]      = 0x00CFFF,  -- primary: bright glowing cyan
+            [colors.white]     = 0xC8E8FF,  -- text: blue-tinted white
+            [colors.lime]      = 0x00E676,  -- success: bright green
+        },
     },
 
 }
