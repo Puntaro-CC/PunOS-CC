7c42c1c78614e5eeb74e2478b976e2c05142248a
Puntaro <punguitar1@gmail.com>
2026-04-29 22:01:50 +0300

    Fix regex pattern in loadTheme.lua

--- os/loadTheme.lua
+++ os/loadTheme.lua
@@ -3,7 +3,7 @@ local f = fs.open("/os/theme.lua", "rb")
 if not f then return dofile("/os/theme.lua") end
 local raw = f.readAll()
 f.close()
-raw = raw:gsub("^\?+", "")
+raw = raw:gsub("^?+", "")
 raw = raw:gsub("\r", "")
 local fn = load(raw, "theme.lua")
 if not fn then return dofile("/os/theme.lua") end
